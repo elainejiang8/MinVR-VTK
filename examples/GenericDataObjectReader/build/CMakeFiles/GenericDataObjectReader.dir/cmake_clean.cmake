74d060ed5885492dc67d246eaec2e7f563c2c565
Elaine Jiang <elaine@Elaines-MacBook-Pro.local>
2017-06-27 11:36:25 -0400

    added 4 data + volume rendering examples

---
+++ examples/GenericDataObjectReader/build/CMakeFiles/GenericDataObjectReader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GenericDataObjectReader.dir/GenericDataObjectReader.cxx.o"
+  "GenericDataObjectReader.pdb"
+  "GenericDataObjectReader.app/Contents/MacOS/GenericDataObjectReader"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GenericDataObjectReader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
