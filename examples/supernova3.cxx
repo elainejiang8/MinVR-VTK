6a80496730ef690ada1b6db0c1d11b52247586f7
Elaine Jiang <elaine@Elaines-MacBook-Pro.local>
2018-12-06 11:22:56 -0500

    updates to repo

--- examples/supernova3.cxx
+++ examples/supernova3.cxx
@@ -364,7 +364,7 @@ private:
 
 	float joystickX, joystickY;
 
-	 DemoVRVTKApp(int argc, char** argv):
+	DemoVRVTKApp(int argc, char** argv):
     
      MVRDemo(argc, argv) {
      movingSlide = false;
@@ -720,40 +720,41 @@ private:
     }
     
 };
+};
 
     
 
-    // The main function is just a shell of its former self.  Just
-    // initializes a MinVR graphics object and runs it.
-    int main(int argc, char **argv) {
-		// Let's see what arguments the user invoked this program with.
-		std::cout << "Invoked with argc=" << argc << " arguments." << std::endl;
-		for (int i = 0; i < argc ; i++) {
-			std::cout << "argv[" << i << "]: " << std::string(argv[i]) << std::endl;
-		}
-
-		// If there weren't enough args, throw an error and explain what the
-		// user should have done.
-
-		if (argc < 2) {
-			throw std::runtime_error("\nNeed a config file.\nTry 'bin/supernova3 -c ../config/desktop-freeglut.xml'");
-		}
-
-		// Is the MINVR_ROOT variable set?  MinVR usually needs this to find
-		// some important things.
-		if (getenv("MINVR_ROOT") == NULL) {
-			std::cout << "***** No MINVR_ROOT -- MinVR might not be found *****" << std::endl 
-					  << "MinVR is found (at runtime) via the 'MINVR_ROOT' variable."
-					  << std::endl << "Try 'export MINVR_ROOT=/my/path/to/MinVR'."
-					  << std::endl;
-		}
-
-		// Initialize the app
-		DemoVRVTKApp app(argc, argv);
-
-		// Run it.
-		app.run();
-
-		// We never get here.
-		return 0;
+// The main function is just a shell of its former self.  Just
+// initializes a MinVR graphics object and runs it.
+int main(int argc, char **argv) {
+	// Let's see what arguments the user invoked this program with.
+	std::cout << "Invoked with argc=" << argc << " arguments." << std::endl;
+	for (int i = 0; i < argc ; i++) {
+		std::cout << "argv[" << i << "]: " << std::string(argv[i]) << std::endl;
 	}
+
+	// If there weren't enough args, throw an error and explain what the
+	// user should have done.
+
+	if (argc < 2) {
+		throw std::runtime_error("\nNeed a config file.\nTry 'bin/supernova3 -c ../config/desktop-freeglut.xml'");
+	}
+
+	// Is the MINVR_ROOT variable set?  MinVR usually needs this to find
+	// some important things.
+	if (getenv("MINVR_ROOT") == NULL) {
+		std::cout << "***** No MINVR_ROOT -- MinVR might not be found *****" << std::endl 
+				  << "MinVR is found (at runtime) via the 'MINVR_ROOT' variable."
+				  << std::endl << "Try 'export MINVR_ROOT=/my/path/to/MinVR'."
+				  << std::endl;
+	}
+
+	// Initialize the app
+	DemoVRVTKApp app(argc, argv);
+
+	// Run it.
+	app.run();
+
+	// We never get here.
+	return 0;
+}
